b71fd31bb695afa4adf46477301e519677cc70c8
Sahli Yandah <sahliyanda.040327@gmail.com>
2026-09-09 18:36:33 +0700

    Praktikum 2: A.2. Struktur Dasar Program Java

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo, Sahli Yandah!");
+
+        System.out.print("Nama: ");
+        System.out.print("Sahli Yandah");
+        System.out.println();
+        System.out.println("Ini Baris Baru");
+
     }
 }
