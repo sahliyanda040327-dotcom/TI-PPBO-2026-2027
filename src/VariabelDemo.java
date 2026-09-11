69e7b7a4224b52bbf00d6631bcd6b7c1b589326f
Sahli Yandah <sahliyanda.040327@gmail.com>
2026-09-11 23:39:00 +0700

    Praktikum 2: B.1. Deklarasi Variabel dan Tipe Data Primitif

---
+++ src/VariabelDemo.java
@@ -0,0 +1,15 @@
+public class VariabelDemo {
+    public static void main(String[] args) {
+        String nama = "Sahli yandah";
+        int umur = 19;
+        double tinggi = 175.0;
+        char golDarah = 'O';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Golongan Darah: " + golDarah);
+        System.out.println("Aktif: " + mahasiswaAktif);
+    }
+}
