d05804c04dfae92771794e58217621e80d808bb6
Sahli Yandah <sahliyanda.040327@gmail.com>
2026-09-13 20:56:39 +0700

    Praktikum 2: B.2. Casting (Konversi Tipe Data)

--- src/VariabelDemo.java
+++ src/VariabelDemo.java
@@ -11,5 +11,15 @@ public class VariabelDemo {
         System.out.println("Tinggi: " + tinggi);
         System.out.println("Golongan Darah: " + golDarah);
         System.out.println("Aktif: " + mahasiswaAktif);
+
+        //widening
+        int nilaiBulat = 9;
+        double nilaiDouble = nilaiBulat; // widening otomatis
+        System.out.println("Widening: " + nilaiDouble);
+
+        //Narrowing
+        double pecahan = 9.8;
+        int hasilCasting = (int) pecahan; // narrowing eksplisit
+        System.out.println("Narrowing: " +hasilCasting);
     }
 }
