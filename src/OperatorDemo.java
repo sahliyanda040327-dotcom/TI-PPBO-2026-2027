ceea7847791f0a725e2e4af68f3a0c84fddfb7a4
Sahli Yandah <sahliyanda.040327@gmail.com>
2026-09-13 21:09:06 +0700

    Praktikum 2: B.3. Operator

---
+++ src/OperatorDemo.java
@@ -0,0 +1,10 @@
+public class OperatorDemo {
+    public static void main(String[] args) {
+        int a = 17 , b = 5;
+        System.out.println("jumlah: " + (a + b));
+        System.out.println("Selisih: " + (a - b));
+        System.out.println("Kali: " + (a * b));
+        System.out.println("Bagi: " + (a / b));
+        System.out.println("Sisa Bagi: " + (a % b));
+    }
+}
