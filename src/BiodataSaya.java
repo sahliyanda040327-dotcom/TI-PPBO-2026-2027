25f843ab199e2d11fafa3ceaf492020b0dbdfbe2
Sahli Yandah <sahliyanda.040327@gmail.com>
2026-09-13 23:11:05 +0700

    Praktikum 2: Latihan 1 Biodata menggunakan println

---
+++ src/BiodataSaya.java
@@ -0,0 +1,15 @@
+//menunjukkan lokasi file dan class BiodataSaya
+public class BiodataSaya {
+    //method utama yang pertama kali jalan
+    public static void main(String[] args) {
+
+        //untuk menampilkan nama di baris pertama
+        System.out.println("Nama: Sahli Yandah");
+
+        //menampilkan Nim di baris kedua
+        System.out.println("Nim: 2025573010025");
+
+        /*menampilkan Prodi di baris ketiga*/
+        System.out.println("Program Studi: Teknik Informatika ");
+    }
+}
