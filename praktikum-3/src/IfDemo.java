a56989c1e30d264a8d982df6017def05ed762dbc
Sahli Yandah <sahliyanda.040327@gmail.com>
2026-09-14 11:49:20 +0700

    praktikum 3: A.2. If-Else

--- praktikum-3/src/IfDemo.java
+++ praktikum-3/src/IfDemo.java
@@ -4,6 +4,8 @@ public class IfDemo {
 
         if (nilai >= 75) {
             System.out.println("Anda LULUS");
+        } else {
+            System.out.println("Anda TIDAK LULUS");
         }
     }
 }
