098b31450b6a9215207f1b4c170802abbbbe9061
Sahli Yandah <sahliyanda.040327@gmail.com>
2026-09-14 12:10:14 +0700

    praktikum 3: B.2. Pentingnya break dan default

---
+++ praktikum-3/src/KalkulatorSwitch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+        public class KalkulatorSwitch {
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Angka pertama: ");
+        double a = sc.nextDouble();
+        System.out.print("Operator (+,-,*,/): ");
+        char op = sc.next().charAt(0);
+        System.out.print("Angka kedua: ");
+        double b = sc.nextDouble();
+
+        double hasil = 0;
+        switch (op) {
+            case '+': hasil = a + b; break;
+            case '-': hasil = a - b; break;
+            case '*': hasil = a * b; break;
+            case '/': hasil = a / b; break;
+            default: System.out.println("Operator tidak dikenali");
+        }
+
+        System.out.println("Hasil: " + hasil);
+        }
+}
